e527f6375e8f472eb7524e1c5364207fc3c83e37
SyedJohn <syedjohnapp@gmail.com>
2023-02-27 20:28:57 +0530

    props demo

--- client/ui/src/App.js
+++ client/ui/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import UsersClass from '../src/UsersClassComp/UsersClass';
+import Users from './Users/Users';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
-      <UsersClass />
+      {/* <UsersClass /> */}
+      <Users  name="anil" age="20" country="india"/>
     </div>
   );
 }
