d6f365de2df2042854754d95c5d8b8ac0dea6229
SyedJohn <syedjohnapp@gmail.com>
2023-02-27 20:45:54 +0530

    useState Hook demo

--- client/ui/src/Users/Users.jsx
+++ client/ui/src/Users/Users.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Users(props) {
+     /* 
     console.log(props);
     const {name, age} = props;
+    */
+   const [name, setName] = useState("john");
+   const changeName = (ev) => {
+    setName(ev.target.value);
+   }
+
   return (
     <>
-    <h1>Users using props</h1>
-    <h3>user name : {name}</h3>
-    <h3>user age : {age}</h3>
+    <h1>Users</h1>
+    <input type="text" placeholder='enter name' onChange={changeName} />
+    <h3>updated name : {name}</h3>
     </>
   )
 }
