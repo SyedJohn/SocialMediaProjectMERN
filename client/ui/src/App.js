9ca58b0eab3c0dd436764902aea5ede56f902bee
SyedJohn <syedjohnapp@gmail.com>
2023-02-27 18:10:17 +0530

    created users class component

--- client/ui/src/App.js
+++ client/ui/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import UsersClass from '../src/UsersClassComp/UsersClass';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <UsersClass />
     </div>
   );
 }
